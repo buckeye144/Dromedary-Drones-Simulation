cc91533d3703f67d855f65228af3164a3c5a206a
buckeye144 <44789411+buckeye144@users.noreply.github.com>
2020-03-30 19:50:30 -0400

    Update Location.java
    finishes the location class

--- Location.java
+++ Location.java
@@ -1,9 +1,36 @@
-public class Location{
-    int xCoordinate;
-    int yCoordinate;
-    
-    public Location(int x, int y){
-        this.xCoordinate = x;
-        this.yCoordinate = y;
-    }
+
+public class location {
+	private String name;
+	private int x;
+	private int y;
+	
+	public location(String name, int x, int y) {
+		this.name = name;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
 }
