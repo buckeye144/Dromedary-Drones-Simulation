a33657d1af2b19ee10a1316ea23b0c763cddb08a
buckeye144 <44789411+buckeye144@users.noreply.github.com>
2020-04-02 14:18:30 -0400

    added xml for the college map and fleshed out the map class

--- map.java
+++ map.java
@@ -1,12 +1,52 @@
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.tools.DocumentationTool.Location;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 
 public class map {
-	private String name;
-	private location waypoints[];
+	private StLocatione;
+	private ArrayList<Location> waypoints;
 	
-	public map(String name, location waypoints[]) {
+	public map() {
+		name = "";
+		waypoints = new ArrayList<Location>();
+	}
+	
+	public map(String name, ArrayList<Location> waypoints) {
 		//creates a map with a provided name and list of locations
 		this.name = name;
 		this.waypoints = waypoints;
+	}
+	
+	public map(File collegeMap) {
+		//Creates a map using a given xml file
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document cMap = dBuilder.parse(collegeMap);
+		cMap.getDocumentElement().normalize();
+		
+		NodeList nList = cMap.getElementsByTagName("location");
+		name = cMap.getElementsByTagName("name").item(0).getTextContent();
+		waypoints = new ArrayList<Location>();
+		
+		for (int i = 0; i < nList.getLength(); i++) {
+			Node nNode = nList.item(i);
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
+				String locName = eElement.getElementsByTagName("locName").item(0).getTextContent();
+				String x = eElement.getElementsByTagName("xCoord").item(0).getTextContent();
+				String y = eElement.getElementsByTagName("yCoord").item(0).getTextContent();
+				int xCoord = Integer.parseInt(x);
+				int yCoord = Integer.parseInt(y);
+				addLocation(locName, xCoord, yCoord);
+			}
+		}
 	}
 	
 	public String getName() {
@@ -16,16 +56,31 @@ public class map {
 	
 	public location getLocation(int i) {
 		//returns a requested location
-		return waypoints[i];
+		return waypoints(i);
 	}
-	/*
-	public location getLocation(String name) {
+	
+	public location getLocation(String locName) {
 		//returns a requested location
-		for (int i = 0; i < waypoints.length; i++) {
-			if (waypoints[i].getName() == name) {
-				return waypoints[i];
+		location reqLoc = null;
+		for (int i = 0; i < waypoints.size(); i++) {
+			if (waypoints(i).getName() == locName) {
+				reqLoc = waypoints(i)
+			}
+		}
+		return reqLoc;
+	}
+	
+	public void addLocation(String locName, int x, int y) {
+		Location newLoc = new Location(locName, x, y);
+		boolean success = waypoints.add(newLoc);
+	}
+	
+	public void removeLocation(String locName) {
+		boolean success;
+		for (int i = 0; i < waypoints.size(); i++) {
+			if (waypoints(i).getName() == locName) {
+				success = waypoints.remove(i);
 			}
 		}
 	}
-	*/
 }
