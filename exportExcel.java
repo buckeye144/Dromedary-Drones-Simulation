db7aa11d5e4faf49f995f84a357dc141a12a132c
buckeye144 <44789411+buckeye144@users.noreply.github.com>
2020-04-09 14:44:29 -0400

    exportExcel does variable arraylists of arraylists

--- exportExcel.java
+++ exportExcel.java
@@ -8,7 +8,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 
 public class exportExcel {
-	public void printExcel(ArrayList<Double> results) {
+	public void printExcel(ArrayList<ArrayList<Double>> results) {
 		//Create blank workbook
 		HSSFWorkbook workbook = new HSSFWorkbook();
 
@@ -17,21 +17,75 @@ public class exportExcel {
 
 		//Create row object
 		HSSFRow row;
-
+		row = spreadsheet.createRow(0);
 		for (int i = 0; i < results.size(); i++) {
-			row = spreadsheet.createRow(i);
-			double time = results.get(i);
+			if (i % 2 == 0) {
+				Cell title = row.createCell(i);
+				title.setCellValue("FIFO");
+			}
+			
+			else {
+				Cell title = row.createCell(i);
+				title.setCellValue("Knapsack");
+			}
+		}
+			
+		/**
+		row = spreadsheet.createRow(0);
+		Cell title = row.createCell(0);
+		title.setCellValue("FIFO");
+		
+		title = row.createCell(1);
+		title.setCellValue("Knapsack");
+		**/
+		
+		for (int i = 0; i < results.get(0).size(); i++) {
+			row = spreadsheet.createRow(i+1);
+			for (int j = 0; j < results.size(); j++) {
+				double time = results.get(j).get(i);
+				Cell cell = row.createCell(j);
+				cell.setCellValue(time);
+			}
+		}	
+			/**
+			row = spreadsheet.createRow(j+1);
+			double timeFIFO = results.get(0).get(j);
+				//double timeKnapsack = results.get(1).get(i);
 			
 			Cell cell = row.createCell(0);
-			cell.setCellValue(time);
+			cell.setCellValue(timeFIFO);
+			
+				//cell = row.createCell(1);
+				//cell.setCellValue(timeKnapsack);
 			
 			/**
 			for (Object obj : objectArr){
 				Cell cell = row.createCell(cellid++);
 				cell.setCellValue((String)obj);
 			}
-			**/
+			
 		}
+		
+		/**
+		for (int j = 0; j < results.get(1).size(); j++) {
+			row = spreadsheet.createRow(j+1);
+			//double timeFIFO = results.get(0).get(j);
+			double timeKnapsack = results.get(1).get(j);
+			
+			//Cell cell = row.createCell(0);
+			//cell.setCellValue(timeFIFO);
+			
+			Cell cell = row.createCell(1);
+			cell.setCellValue(timeKnapsack);
+			
+			/**
+			for (Object obj : objectArr){
+				Cell cell = row.createCell(cellid++);
+				cell.setCellValue((String)obj);
+			}
+			
+		}
+		**/
 		//Write the workbook in file system
 		try {
 			// System.out.println("Creating output stream");
