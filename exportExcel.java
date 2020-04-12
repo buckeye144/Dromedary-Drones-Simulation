b15384c4adbbc98e552935be3e3dd9dec202040c
buckeye144 <44789411+buckeye144@users.noreply.github.com>
2020-04-11 21:17:22 -0400

    reworked exportExcel to create graph in the results spreadsheet and added the poi, commons math, commons collection, commons compress, and xmlbeans libraries to allow it to function.

--- exportExcel.java
+++ exportExcel.java
@@ -1,95 +1,105 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
+
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xddf.usermodel.chart.AxisPosition;
+import org.apache.poi.xddf.usermodel.chart.ChartTypes;
+import org.apache.poi.xddf.usermodel.chart.LegendPosition;
+import org.apache.poi.xddf.usermodel.chart.MarkerStyle;
+import org.apache.poi.xddf.usermodel.chart.XDDFCategoryAxis;
+import org.apache.poi.xddf.usermodel.chart.XDDFChartLegend;
+import org.apache.poi.xddf.usermodel.chart.XDDFDataSource;
+import org.apache.poi.xddf.usermodel.chart.XDDFDataSourcesFactory;
+import org.apache.poi.xddf.usermodel.chart.XDDFScatterChartData;
+import org.apache.poi.xddf.usermodel.chart.XDDFNumericalDataSource;
+import org.apache.poi.xddf.usermodel.chart.XDDFValueAxis;
+import org.apache.poi.xssf.usermodel.XSSFChart;
+import org.apache.poi.xssf.usermodel.XSSFClientAnchor;
+import org.apache.poi.xssf.usermodel.XSSFDrawing;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
 public class exportExcel {
 	public void printExcel(ArrayList<ArrayList<Double>> results) {
 		//Create blank workbook
-		HSSFWorkbook workbook = new HSSFWorkbook();
+		XSSFWorkbook workbook = new XSSFWorkbook();
 
 		//Create a blank sheet
-		HSSFSheet spreadsheet = workbook.createSheet( " time results ");
+		XSSFSheet spreadsheet = workbook.createSheet( " time results ");
 
 		//Create row object
-		HSSFRow row;
+		Row row;
 		row = spreadsheet.createRow(0);
+		
+		XSSFDrawing drawing = spreadsheet.createDrawingPatriarch();
+		XSSFClientAnchor anchor = drawing.createAnchor(0, 0, 0, 0, 0, 4, 7, 26);
+		
+		XSSFChart chart = drawing.createChart(anchor);
+		chart.setTitleText("Delivery Times of Orders");
+		chart.setTitleOverlay(false);
+		
+		XDDFCategoryAxis xAxis = chart.createCategoryAxis(AxisPosition.BOTTOM);
+		xAxis.setTitle("Delivery Times (min)");
+		XDDFValueAxis yAxis = chart.createValueAxis(AxisPosition.LEFT);
+		yAxis.setTitle("Order Number");
+		
+		XDDFScatterChartData data = (XDDFScatterChartData) chart.createData(ChartTypes.SCATTER, xAxis, yAxis);
+		
+		//Creates column titles
 		for (int i = 0; i < results.size(); i++) {
 			if (i % 2 == 0) {
-				Cell title = row.createCell(i);
-				title.setCellValue("FIFO");
+				Cell title = row.createCell(i+1);
+				title.setCellValue("FIFO " + i + 1);
 			}
 			
 			else {
-				Cell title = row.createCell(i);
-				title.setCellValue("Knapsack");
+				Cell title = row.createCell(i+1);
+				title.setCellValue("Knapsack " + i);
 			}
 		}
-			
-		/**
-		row = spreadsheet.createRow(0);
-		Cell title = row.createCell(0);
-		title.setCellValue("FIFO");
-		
-		title = row.createCell(1);
-		title.setCellValue("Knapsack");
-		**/
 		
 		for (int i = 0; i < results.get(0).size(); i++) {
 			row = spreadsheet.createRow(i+1);
+			Cell order = row.createCell(0);
+			order.setCellValue(i+1);
+		}
+		
+		XDDFDataSource<String> orderNum = XDDFDataSourcesFactory.fromStringCellRange(spreadsheet, new CellRangeAddress(1,results.get(0).size(),0,0));
+		//Builds spreadsheet
+		for (int i = 0; i < results.get(0).size(); i++) {
+			row = spreadsheet.getRow(i+1);
 			for (int j = 0; j < results.size(); j++) {
 				double time = results.get(j).get(i);
-				Cell cell = row.createCell(j);
+				//System.out.println(time);
+				Cell cell = row.createCell(j+1);
 				cell.setCellValue(time);
 			}
 		}	
-			/**
-			row = spreadsheet.createRow(j+1);
-			double timeFIFO = results.get(0).get(j);
-				//double timeKnapsack = results.get(1).get(i);
+		
+		for (int i = 0; i < results.size(); i++) {
+			XDDFNumericalDataSource<Double> times = XDDFDataSourcesFactory.fromNumericCellRange(spreadsheet, new CellRangeAddress(1, results.get(0).size(), i, i));
+			XDDFScatterChartData.Series series = (XDDFScatterChartData.Series) data.addSeries(orderNum, times);
 			
-			Cell cell = row.createCell(0);
-			cell.setCellValue(timeFIFO);
-			
-				//cell = row.createCell(1);
-				//cell.setCellValue(timeKnapsack);
-			
-			/**
-			for (Object obj : objectArr){
-				Cell cell = row.createCell(cellid++);
-				cell.setCellValue((String)obj);
+			if (i % 2 == 0) {
+				series.setTitle("Fifo " + i + 1, null);
 			}
 			
+			else {
+				series.setTitle("Knapsack " + i, null);
+			}
 		}
 		
-		/**
-		for (int j = 0; j < results.get(1).size(); j++) {
-			row = spreadsheet.createRow(j+1);
-			//double timeFIFO = results.get(0).get(j);
-			double timeKnapsack = results.get(1).get(j);
-			
-			//Cell cell = row.createCell(0);
-			//cell.setCellValue(timeFIFO);
-			
-			Cell cell = row.createCell(1);
-			cell.setCellValue(timeKnapsack);
-			
-			/**
-			for (Object obj : objectArr){
-				Cell cell = row.createCell(cellid++);
-				cell.setCellValue((String)obj);
-			}
-			
-		}
-		**/
+		chart.plot(data);
+		
 		//Write the workbook in file system
 		try {
 			// System.out.println("Creating output stream");
-			FileOutputStream out = new FileOutputStream(new File("results.xls"));
+			FileOutputStream out = new FileOutputStream(new File("results.xlsx"));
 			// System.out.println("Writing workbook");
 			workbook.write(out);
 			// System.out.println("Workbook written");
