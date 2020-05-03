c47c38750dabec4384ad6dac433964b8c4636918
buckeye144 <44789411+buckeye144@users.noreply.github.com>
2020-05-03 19:26:49 -0400

    jars has been simplified

--- exportExcel.java
+++ exportExcel.java
@@ -1,20 +1,19 @@
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xddf.usermodel.chart.AxisPosition;
+import org.apache.poi.xddf.usermodel.chart.BarDirection;
 import org.apache.poi.xddf.usermodel.chart.ChartTypes;
 import org.apache.poi.xddf.usermodel.chart.LegendPosition;
-import org.apache.poi.xddf.usermodel.chart.MarkerStyle;
+import org.apache.poi.xddf.usermodel.chart.XDDFBarChartData;
 import org.apache.poi.xddf.usermodel.chart.XDDFCategoryAxis;
+import org.apache.poi.xddf.usermodel.chart.XDDFChartData;
 import org.apache.poi.xddf.usermodel.chart.XDDFChartLegend;
 import org.apache.poi.xddf.usermodel.chart.XDDFDataSource;
 import org.apache.poi.xddf.usermodel.chart.XDDFDataSourcesFactory;
-import org.apache.poi.xddf.usermodel.chart.XDDFScatterChartData;
 import org.apache.poi.xddf.usermodel.chart.XDDFNumericalDataSource;
 import org.apache.poi.xddf.usermodel.chart.XDDFValueAxis;
 import org.apache.poi.xssf.usermodel.XSSFChart;
@@ -23,9 +22,14 @@ import org.apache.poi.xssf.usermodel.XSSFDrawing;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-
 public class exportExcel {
 	public void printExcel(ArrayList<ArrayList<Double>> results) {
+		double fifoMax = results.get(0).get(results.get(0).size() - 1);
+		double knapsackMax = results.get(1).get(results.get(1).size() - 1);
+		double maxTime = Math.max(fifoMax, knapsackMax); //find the max time it took
+		double FIFOAverage = 0;
+		double KnapsackAverage = 0;
+		
 		//Create blank workbook
 		XSSFWorkbook workbook = new XSSFWorkbook();
 
@@ -36,79 +40,131 @@ public class exportExcel {
 		Row row;
 		row = spreadsheet.createRow(0);
 		
-		//Assigns where in the document the chart will appear
-		XSSFDrawing drawing = spreadsheet.createDrawingPatriarch();
-		XSSFClientAnchor anchor = drawing.createAnchor(0, 0, 0, 0, 0, 4, 7, 26);
-		
-		//Creates the Chart and assigns a title
-		XSSFChart chart = drawing.createChart(anchor);
-		chart.setTitleText("Delivery Times of Orders");
-		chart.setTitleOverlay(false);
-		
-		//Creates the x and y axes and their titles
-		XDDFCategoryAxis xAxis = chart.createCategoryAxis(AxisPosition.BOTTOM);
-		xAxis.setTitle("Delivery Times (min)");
-		XDDFValueAxis yAxis = chart.createValueAxis(AxisPosition.LEFT);
-		yAxis.setTitle("Order Number");
-		
-		//Adds the axes to the chart
-		XDDFScatterChartData data = (XDDFScatterChartData) chart.createData(ChartTypes.SCATTER, xAxis, yAxis);
-		
 		//Creates column titles
-		for (int i = 0; i < results.size(); i++) {
-			
-			if (i % 2 == 0) {
-				Cell title = row.createCell(i+1);
-				title.setCellValue("FIFO " + i + 1);
-			}
-			
-			else {
-				Cell title = row.createCell(i+1);
-				title.setCellValue("Knapsack " + i);
-			}
-		}
-		
-		//Sets order numbers
-		for (int i = 0; i < results.get(0).size(); i++) {
-			row = spreadsheet.createRow(i+1);
-			Cell order = row.createCell(0);
-			order.setCellValue(i+1);
-		}
-		
-		XDDFDataSource<String> orderNum = XDDFDataSourcesFactory.fromStringCellRange(spreadsheet, new CellRangeAddress(1,results.get(0).size(),0,0));
+		Cell title = row.createCell(1);
+		title.setCellValue("FIFO");
+		title = row.createCell(2);
+		title.setCellValue("Knapsack");
 		
 		//Builds spreadsheet
 		for (int i = 0; i < results.get(0).size(); i++) {
 			row = spreadsheet.getRow(i+1);
 			for (int j = 0; j < results.size(); j++) {
 				double time = results.get(j).get(i);
+				//System.out.println(time);
 				Cell cell = row.createCell(j+1);
 				cell.setCellValue(time);
 			}
-		}	
+		}
 		
-		//Adds data series to the chart
-		for (int i = 0; i < results.size(); i++) {
-			XDDFNumericalDataSource<Double> times = XDDFDataSourcesFactory.fromNumericCellRange(spreadsheet, new CellRangeAddress(1, results.get(0).size(), i, i));
-			XDDFScatterChartData.Series series = (XDDFScatterChartData.Series) data.addSeries(orderNum, times);
-			
-			if (i % 2 == 0) {
-				series.setTitle("Fifo " + i + 1, null);
-			}
-			
-			else {
-				series.setTitle("Knapsack " + i, null);
+		for (int i = 0; i < results.get(0).size(); i++) {
+			row = spreadsheet.createRow(i+1);
+			Cell order = row.createCell(0);
+			order.setCellValue(i+1);
+		}
+		
+		XSSFDrawing drawing = spreadsheet.createDrawingPatriarch();
+		XSSFClientAnchor anchor = drawing.createAnchor(0, 0, 0, 0, 0, 4, 7, 26);
+		
+		XSSFChart chart = drawing.createChart(anchor);
+		chart.setTitleText("Drone Simulation");
+		chart.setTitleOverlay(false);
+		
+		//Creates the legend
+		XDDFChartLegend legend = chart.getOrAddLegend();
+		legend.setPosition(LegendPosition.TOP_RIGHT);
+		
+		//Sets axis titles
+		XDDFCategoryAxis xAxis = chart.createCategoryAxis(AxisPosition.BOTTOM);
+		xAxis.setTitle("Turn Around Time (in minutes)");
+		XDDFValueAxis yAxis = chart.createValueAxis(AxisPosition.LEFT);
+		yAxis.setTitle("# of Orders");
+		
+		//Create data series for FIFO orders
+		int curOrder = 0; //keep track of what order you're on
+		ArrayList<Integer> FIFOorders = new ArrayList<>();
+		ArrayList<String> timeRange = new ArrayList<>();
+		for (Integer min = 1; min < maxTime; min++) {
+			int numOrders = 0;
+			while(curOrder < results.get(0).size()){
+				if(results.get(0).get(curOrder) < min){
+					FIFOAverage += results.get(0).get(curOrder);
+					curOrder++;
+					numOrders++;
+				}
+				else{
+					FIFOorders.add(numOrders);
+					timeRange.add(min.toString());
+					break;
+				}
 			}
 		}
 		
-		//Plots the data on the chart
+		Integer[] FIFOarray = FIFOorders.toArray(new Integer[FIFOorders.size()]);
+		String[] timeArray = timeRange.toArray(new String[timeRange.size()]);
+		XDDFNumericalDataSource<Integer> FIFOnumOrders = XDDFDataSourcesFactory.fromArray(FIFOarray);
+		XDDFDataSource<String> times = XDDFDataSourcesFactory.fromArray(timeArray);
+		FIFOAverage = FIFOAverage / (double)results.get(0).size();
+		
+		//Create data series for Knapsack orders
+        curOrder = 0; //keep track of what order you're on
+        ArrayList<Integer> KnapsackOrders = new ArrayList<>();
+		for (int min = 1; min < maxTime; min++) {
+			int numOrders = 0;
+			while(curOrder < results.get(0).size()){
+				if(results.get(1).get(curOrder) < min){
+					KnapsackAverage += results.get(1).get(curOrder);
+					curOrder++;
+					numOrders++;
+				}
+				else{
+					KnapsackOrders.add(numOrders);
+					break;
+				}
+			}
+		}
+		Integer[] KnapsackArray = KnapsackOrders.toArray(new Integer[KnapsackOrders.size()]);
+		XDDFNumericalDataSource<Integer> KnapsackNumOrders = XDDFDataSourcesFactory.fromArray(KnapsackArray);
+		KnapsackAverage = KnapsackAverage / (double)results.get(1).size();
+		
+		//Create data series and chart them
+		XDDFChartData data = chart.createData(ChartTypes.BAR, xAxis, yAxis);
+		XDDFChartData.Series FIFOseries = data.addSeries(times, FIFOnumOrders);
+		XDDFChartData.Series KnapsackSeries = data.addSeries(times, KnapsackNumOrders);
+		FIFOseries.setTitle("FIFO", null);
+		KnapsackSeries.setTitle("Knapsack", null);
+		data.setVaryColors(true);
 		chart.plot(data);
+		
+		//Turn chart into a bar chart
+		XDDFBarChartData bar = (XDDFBarChartData) data;
+		bar.setBarDirection(BarDirection.COL);
+		
+		//Writes the average and worst times to a spreadsheet
+		XSSFSheet averageWorst = workbook.createSheet(" average and worst times ");
+		Row fifo = averageWorst.createRow(0);
+		Row knapsack = averageWorst.createRow(1);
+		
+		//Calculate the averages for FIFO and Knapsack
+		Cell cell = fifo.createCell(0);
+		cell.setCellValue("FIFO Average: ");
+		cell = fifo.createCell(1);
+		cell.setCellValue(FIFOAverage);
+		
+		cell = knapsack.createCell(0);
+		cell.setCellValue("Knapsack Average: ");
+		cell = knapsack.createCell(1);
+		cell.setCellValue(KnapsackAverage);
 		
 		//Write the workbook in file system
 		try {
+			// System.out.println("Creating output stream");
 			FileOutputStream out = new FileOutputStream(new File("results.xlsx"));
+			// System.out.println("Writing workbook");
 			workbook.write(out);
+			// System.out.println("Workbook written");
 			out.close();
+			// System.out.println("results.xls written successfully");
 			workbook.close();
 		} catch (Exception e) {
 			System.out.println(e);
